0eaf0cbf3d05c5888109f250f381d8dd1ff94ba3
minging <jinbabging@gmail.com>
2024-09-23 09:19:28 +0900

    9월 23일 unversioned Files 공유

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:forestring_teach/Data/constant.dart';
 import 'package:forestring_teach/Intro_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:forestring_teach/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
 
   runApp(const Forestring());
 }
