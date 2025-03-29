74a6fa91b414215adb8595efd18d089f7180382b
minging <jinbabging@gmail.com>
2025-03-29 14:36:10 +0900

    ✨ 수업 자동 생성 함수 (autoFillFutureLessons) 추가

--- functions/index.js
+++ functions/index.js
@@ -6,3 +6,7 @@ admin.initializeApp();
 // 학생 아카이빙 함수 가져오기
 const { autoArchiveStudents } = require("./archive/autoArchive");
 exports.autoArchiveStudents = autoArchiveStudents;
+
+// 수업 자동 생성 함수
+const { autoFillFutureLessons } = require("./lesson/autoFillFutureLessons");
+exports.autoFillFutureLessons = autoFillFutureLessons;
