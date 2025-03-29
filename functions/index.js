fe4b745851e8d7eb2b8baaed03397aa06f92550b
minging <jinbabging@gmail.com>
2025-03-29 13:21:24 +0900

    feat: 자동 학생 아카이빙 함수 구현

--- functions/index.js
+++ functions/index.js
@@ -1,22 +1,8 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions/v1");
+const admin = require("firebase-admin");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+admin.initializeApp();
 
-const functions = require("firebase-functions/v1")
-
-exports.helloWorld = functions.region("asia-northeast3").https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-});
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+// 학생 아카이빙 함수 가져오기
+const { autoArchiveStudents } = require("./archive/autoArchive");
+exports.autoArchiveStudents = autoArchiveStudents;
